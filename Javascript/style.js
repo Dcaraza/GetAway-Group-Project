2393734aa498f5a5de05b2b4abffbad353b39d6a
curiouscat21 <daniela.s.treminio@gmail.com>
2019-05-01 16:10:59 -0400

    styled the slider, styled initial icons, added a map using google maps api

--- Javascript/style.js
+++ Javascript/style.js
@@ -14,23 +14,6 @@ M.Slider.init(slider, {
 
 });
 
-//Autocomplete
-
-const ac = document.querySelector(".autocomplete");
-M.Autocomplete.init(ac, {
-    data: {
-        "New York": null,
-        "Paris, France": null,
-        "Egypt": null,
-        "Tokyo": null,
-        "Rio de Janeiro": null,
-        "Italy": null,
-        "Miami, FL": null,
-        "Hawaii": null,
-        "Cuba": null,
-        "Hialeah": null,
-    }
-})
 
 //Material Box
 
@@ -41,3 +24,72 @@ M.Materialbox.init(mb, {});
 
 const ss = document.querySelectorAll(".scrollspy");
 M.ScrollSpy.init(ss, {});
+
+
+//Map
+
+var map;
+
+function createMap(){
+  var options = {
+    center: {
+      lat: 43.654, 
+      lng: -79.383, },
+
+    zoom: 10
+  };
+
+  map = new google.map.createMap(document.getElementById("map"), options);
+}
+
+
+//Logic for displaying Map
+
+var map;
+
+function createMap () {
+  var options = {
+    center: { lat: 25.7617, lng: -80.1918 }, 
+    zoom: 10
+  };
+
+  map = new google.maps.Map(document.getElementById('map'), options);
+
+  var input = document.getElementById('search-map');
+  var searchBox = new google.maps.places.SearchBox(input);
+
+  map.addListener('bounds_changed', function() {
+    searchBox.setBounds(map.getBounds());
+  });
+
+  var markers = [];
+  
+  searchBox.addListener('places_changed', function () {
+    var places = searchBox.getPlaces();
+
+    if (places.length == 0)
+      return;
+
+    markers.forEach(function (m) { m.setMap(null); });
+    markers = [];
+
+    var bounds = new google.maps.LatLngBounds();
+    places.forEach(function(p) {
+      if (!p.geometry)
+        return;
+
+      markers.push(new google.maps.Marker({
+        map: map,
+        title: p.name,
+        position: p.geometry.location
+      }));
+
+      if (p.geometry.viewport)
+        bounds.union(p.geometry.viewport);
+      else
+        bounds.extend(p.geometry.location);
+    });
+    
+    map.fitBounds(bounds);
+  });
+}  
