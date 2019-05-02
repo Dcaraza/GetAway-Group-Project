b90f34e402c5561f0706c581199fec33df32dedb
curiouscat21 <daniela.s.treminio@gmail.com>
2019-05-01 21:33:31 -0400

    style finalized

--- Javascript/logic.js
+++ Javascript/logic.js
@@ -3,85 +3,85 @@ $(document).ready(function () {
 
     var APIKey = "de42150348a73a03e431860aa9d9a03b"
 
-        function locationDetails() {
-            $('.btn').on("click", function (event) {
-                // event.preventdefault();
-                console.log("check");
-                var n = $("#search-map").val();
-                console.log(n);
+    function locationDetails() {
+        $('.btn').on("click", function (event) {
+            // event.preventdefault();
+            console.log("check");
+            var n = $("#search-map").val();
+            console.log(n);
 
-                place = n
-                var newURL = "http://api.openweathermap.org/data/2.5/weather?q=" + place + "&units=standard" + "&APPID=" + APIKey;
+            place = n
+            var newURL = "http://api.openweathermap.org/data/2.5/weather?q=" + place + "&units=standard" + "&APPID=" + APIKey;
 
-                $.ajax({
-                    url: newURL,
-                    method: 'GET',
+            $.ajax({
+                url: newURL,
+                method: 'GET',
 
-                }).then(function (response) {
+            }).then(function (response) {
 
-                    var temp= response.main.temp
-                    var tempF = Math.floor(((temp-273.15)*1.8)+32) + "°F";
-                  
-                    var humidity = response.main.humidity;
-                    var wind = response.wind.speed;
-                    
-                    console.log(response);
-                    console.log(response.weather[0].description);
-                    
-                    $("#temp").text("Temperature: " + tempF);
-                    $("#humidity").text("Humidity: " + humidity);
-                    $("#wind").text("Wind Speed: " + wind + " mph");
-                   
-                
-                    
-                });
+                var temp = response.main.temp
+                var tempF = Math.floor(((temp - 273.15) * 1.8) + 32) + "°F";
+
+                var humidity = response.main.humidity;
+                var wind = response.wind.speed;
+
+                console.log(response);
+                console.log(response.weather[0].description);
+
+                $("#temp").text("Temperature: " + tempF);
+                $("#humidity").text("Humidity: " + humidity);
+                $("#wind").text("Wind Speed: " + wind + " mph");
+
+
+
             });
-        }
-        locationDetails(); 
-        
-        var APIKey = "de42150348a73a03e431860aa9d9a03b"
+        });
+    }
+    locationDetails();
 
-        function flightsDetails() {
-            $('#flights').on("click", function (event) {
-                // event.preventdefault();
-                console.log("check");
-                var n = $("#autocomplete-input").val();
-                console.log(n);
+    var APIKey = "de42150348a73a03e431860aa9d9a03b"
 
-                place = n
-                var newURL = "http://api.openweathermap.org/data/2.5/weather?q=" + place + "&units=standard" + "&APPID=" + APIKey;
+    function flightsDetails() {
+        $('#flights').on("click", function (event) {
+            // event.preventdefault();
+            console.log("check");
+            var n = $("#autocomplete-input").val();
+            console.log(n);
 
-                $.ajax({
-                    url: newURL,
-                    method: 'GET',
+            place = n
+            var newURL = "http://api.openweathermap.org/data/2.5/weather?q=" + place + "&units=standard" + "&APPID=" + APIKey;
 
-                }).then(function (response) {
-                    console.log(response)
-                });
+            $.ajax({
+                url: newURL,
+                method: 'GET',
+
+            }).then(function (response) {
+                console.log(response)
             });
-        }
-        flightsDetails(); 
+        });
+    }
+    flightsDetails();
 
-        var APIKey = "de42150348a73a03e431860aa9d9a03b"
+    var APIKey = "de42150348a73a03e431860aa9d9a03b"
 
-        function travelDetails() {
-            $('#travel').on("click", function (event) {
-                // event.preventdefault();
-                console.log("check");
-                var n = $("#autocomplete-input").val();
-                console.log(n);
+    function travelDetails() {
+        $('#travel').on("click", function (event) {
+            // event.preventdefault();
+            console.log("check");
+            var n = $("#autocomplete-input").val();
+            console.log(n);
 
-                place = n
-                var newURL = "http://api.openweathermap.org/data/2.5/weather?q=" + place + "&units=standard" + "&APPID=" + APIKey;
+            place = n
+            var newURL = "http://api.openweathermap.org/data/2.5/weather?q=" + place + "&units=standard" + "&APPID=" + APIKey;
 
-                $.ajax({
-                    url: newURL,
-                    method: 'GET',
+            $.ajax({
+                url: newURL,
+                method: 'GET',
 
-                }).then(function (response) {
-                    console.log(response)
-                });
+            }).then(function (response) {
+                console.log(response)
             });
-        }
-        travelDetails(); 
+        });
+    }
+    travelDetails();
 });
